b9c74f70a495bb9ec9fcb8e45b2d15e28f8ec7b3
anthonypileggi <anthony@ifixit.com>
2018-11-27 09:41:39 -0500

    update 'play' function

--- R/play.R
+++ R/play.R
@@ -1,11 +1,15 @@
 #' Start a shiny game
 #' @param game which game do you want to play?
 #' @export
-play <- function(game) {
-  app_dir <- system.file("shiny", game, package = "gamer")
-  if (app_dir == "") {
-    message(paste("There is no game called ", game, ". Please try again.\n"))
+play <- function(game = NULL) {
+  shiny_dir <- system.file("shiny", package = "gamer")
+  games <- list.dirs(shiny_dir, full.names = FALSE)[-1]
+  game_msg <- paste("Available Games:\n\n", paste(games, collapse = "\n"), "\n")
+  if (is.null(game)) {
+    message(game_msg)
+  } else if (!is.element(game, games)) {
+    message(paste("There is no game called ", game, ".\n\n", game_msg))
   } else {
-    shiny::runApp(app_dir, display.mode = "normal")
+    shiny::runApp(file.path(shiny_dir, game), display.mode = "normal")
   }
 }
